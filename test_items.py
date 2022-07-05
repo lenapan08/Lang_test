a414738c9e5fae061b0109d6a7736f43232dcade
lenapan08 <83510476+lenapan08@users.noreply.github.com>
2022-07-05 13:43:47 +0600

    Delete test_items.py

--- test_items.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-def test_check_user_lang(browser):
-    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    button = browser.find_element(By.CSS_SELECTOR, "button.[btn.btn-lg.btn-primary.btn-add-to-basket]")
-    assert button >0,  "Button not found"
